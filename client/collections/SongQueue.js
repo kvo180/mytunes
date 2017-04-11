c3d8a1302f56ce1abedd6787e848930624bf6da4
Hack Reactor Student <student@hackreactor.com>
2017-04-11 11:57:32 -0700

    Completed Song Queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,30 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
-  initialize: function() {
-  }
+  initialize: function() {  
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
 
+    this.on('ended', function(song) {
+      // if song ends, remove from models
+      this.shift();
+      // check to see if any other songs in queue
+        // if yes, play that song
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(song) {
+      // if song is clicked in queue, song must be removed from list
+      this.remove(song);
+    }, this);
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  }
 });
